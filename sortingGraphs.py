8f9cc11495ba1f8fcf6c4caf8aca2ad59b8a6f17
Skeasty <lpiloco@uvm.edu>
2023-03-26 13:58:55 -0400

    Graph formatting

--- sortingGraphs.py
+++ sortingGraphs.py
@@ -1,12 +1,25 @@
 import matplotlib.pyplot as plt
 
 file = open("../data/bubbleData.txt", 'r')
-ax = plt.subplot(111)
 reads = []
 writes = []
 for line in file:
     line = line.split(" ")
-    line[3] = line[3].strip('\n')
-    print(line)
+    line[2] = line[2].strip('\n')
+    if line[1] == 'reads':
+        reads.append(int(line[2]))
+    else:
+        writes.append(int(line[2]))
 
 file.close()
+
+ax = plt.subplot(111)
+sizes = range(85, 1001, 100)
+reads.reverse()
+ax.bar(sizes, reads, width=30, color='r', align='center')
+
+plt.title('BubbleSort Reads and Writes(by vector size)')
+plt.xlabel('Vector Size')
+plt.ylabel('Number of Operations')
+ax.legend()
+plt.show()
