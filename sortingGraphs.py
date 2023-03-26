fcab453fc940e58352fd4b6b9870afe7be7f0edc
Sean-300 <sstclair@uvm.edu>
2023-03-26 14:06:53 -0400

    Added a space to the graph title

--- sortingGraphs.py
+++ sortingGraphs.py
@@ -18,7 +18,7 @@ sizes = range(85, 1001, 100)
 reads.reverse()
 ax.bar(sizes, reads, width=30, color='r', align='center')
 
-plt.title('BubbleSort Reads and Writes(by vector size)')
+plt.title('BubbleSort Reads and Writes (by vector size)')
 plt.xlabel('Vector Size')
 plt.ylabel('Number of Operations')
 ax.legend()
